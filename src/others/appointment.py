250b8334770073013a38c3f8f50ae1cac233546f
SathishDuraiyan <2k21cse132@kiot.ac.in>
2024-04-04 14:07:44 +0530

    new repo

--- src/others/appointment.py
+++ src/others/appointment.py
@@ -9,4 +9,16 @@ def Appointment(cursor, accessLevel):
     if accessLevel == "doctor":
         st.subheader("Doctor Appointments")
     elif accessLevel == "nurse":
-        st.subheader("Nurse Appointments")
+
+        file_upload  = st.file_uploader("Appointments Upload", type=['xlsx'])
+
+        if file_upload is not None:
+            df = p.read_excel(file_upload, dtype={'Phone (Personal)': str, 'Emergency Contact  phone':str})
+            st.write(df)
+
+            if st.button("Submit"):
+                st.write("Data Submitted")
+                # convert the df to json
+                data = df.to_dict(orient='records')
+                for i in data:
+                    st.write(i)
