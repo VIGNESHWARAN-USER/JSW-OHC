0bae49cdf050a83735025085bce819bde7d5c761
Shabari_K_S <shabaricse2003@gmail.com>
2024-04-05 15:04:10 +0530

    requirement update

--- src/others/search.py
+++ src/others/search.py
@@ -3,16 +3,25 @@ import os
 import pandas as pd
 from streamlit_modal import Modal
 
+def show_data(emp):
+    # show name and id and other details with one view button
+    for i in range(len(emp)):
+        with st.container(border=1):
+            st.write(emp[i]["emp_no"], emp[i]["name"])
+            if st.button("View", key=i):
+                st.session_state.open_modal = True
+                st.session_state.usr_prof = emp
+
 def set_data(emp):
-    st.session_state.data = emp.to_dict('records')[0]
+    st.session_state.data = emp.to_dict('records')
 
 def Search(cursor):
+    if "usr_prof" not in st.session_state:
+        st.session_state.usr_prof = {}
     if "search" not in st.session_state:
         st.session_state.search = False
     if "searchinp" not in st.session_state:
         st.session_state.searchinp = ""
-    if "view" not in st.session_state:
-        st.session_state.view = False
 
     if "data" not in st.session_state:
         st.session_state.data = {}
@@ -28,7 +37,7 @@ def Search(cursor):
         st.session_state.search = st.button("Search")
 
     if st.session_state.search:
-        cursor.execute(f"SELECT * FROM Employee_det WHERE emp_no = '{st.session_state.searchinp}'")
+        cursor.execute(f"SELECT * FROM Employee_det WHERE emp_no like '%{st.session_state.searchinp}%' ")
 
         emp = pd.DataFrame(cursor.fetchall(), columns=[desc[0] for desc in cursor.description])
         if emp.empty:
@@ -36,4 +45,24 @@ def Search(cursor):
         else:
             set_data(emp)
     
-    st.write(st.session_state.data)
+    st.write(pd.DataFrame(st.session_state.data))
+    show_data(st.session_state.data)
+
+    modal = Modal(
+        "Profile", 
+        key="profile-modal",
+        padding=20,
+        max_width=744
+    )
+    if st.session_state.open_modal:
+        modal.open()
+
+    if modal.is_open():
+        with modal.container():
+            col1, col2 = st.columns(2)
+            with col1:
+                st.write("Sidebar content goes here")
+            with col2:
+                st.write("Main window content goes here")
+    else:
+        st.session_state.open_modal = False  # Set open_modal to False when modal is closed
