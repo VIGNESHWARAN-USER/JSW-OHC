aae703fec4c6a7c2ac56a58f80ad1adb5990e3dc
Shabari_K_S <shabaricse2003@gmail.com>
2024-07-06 11:30:09 +0530

    old

--- src/others/search.py
+++ src/others/search.py
@@ -3,19 +3,25 @@ import os
 import pandas as pd
 from streamlit_modal import Modal
 
+
 def show_data(emp):
     # show name and id and other details with one view button
     for i in range(len(emp)):
         with st.container(border=1):
             st.write(emp[i]["emp_no"], emp[i]["name"])
-            if st.button("View", key=i):
+            if st.button("View",key=i):
                 st.session_state.open_modal = True
-                st.session_state.usr_prof = emp
+                st.session_state.usr_prof = emp[i]
+                st.rerun()
 
 def set_data(emp):
     st.session_state.data = emp.to_dict('records')
 
 def Search(cursor):
+    modal = Modal(
+        "Employee Profile",
+        key="modal",
+    )
     if "usr_prof" not in st.session_state:
         st.session_state.usr_prof = {}
     if "search" not in st.session_state:
@@ -28,41 +34,31 @@ def Search(cursor):
     if "open_modal" not in st.session_state:
         st.session_state.open_modal = False
 
-    st.title("Search")
-    search1, search2 = st.columns([8,2])
-    with search1:
-        st.session_state.searchinp = st.text_input("search",placeholder="Search by Patient ID")
-    with search2:
-        st.write("<div><br></div>", unsafe_allow_html=True)
-        st.session_state.search = st.button("Search")
+    if st.session_state.open_modal == False:
+        st.title("Search")
+        search1, search2 = st.columns([8,2])
+        with search1:
+            st.session_state.searchinp = st.text_input("search",placeholder="Search by Patient ID")
+        with search2:
+            st.write("<div><br></div>", unsafe_allow_html=True)
+            st.session_state.search = st.button("Search")
 
-    if st.session_state.search:
-        cursor.execute(f"SELECT * FROM Employee_det WHERE emp_no like '%{st.session_state.searchinp}%' ")
+        if st.session_state.search:
+            cursor.execute(f"SELECT * FROM Employee_det WHERE emp_no like '%{st.session_state.searchinp}%' ")
 
-        emp = pd.DataFrame(cursor.fetchall(), columns=[desc[0] for desc in cursor.description])
-        if emp.empty:
-            st.error("No records found")
-        else:
-            set_data(emp)
-    
-    st.write(pd.DataFrame(st.session_state.data))
-    show_data(st.session_state.data)
+            emp = pd.DataFrame(cursor.fetchall(), columns=[desc[0] for desc in cursor.description])
+            if emp.empty:
+                st.error("No records found")
+            else:
+                set_data(emp)
 
-    modal = Modal(
-        "Profile", 
-        key="profile-modal",
-        padding=20,
-        max_width=744
-    )
-    if st.session_state.open_modal:
-        modal.open()
+        st.write(pd.DataFrame(st.session_state.data))
+        show_data(st.session_state.data)
 
-    if modal.is_open():
-        with modal.container():
-            col1, col2 = st.columns(2)
-            with col1:
-                st.write("Sidebar content goes here")
-            with col2:
-                st.write("Main window content goes here")
+
     else:
-        st.session_state.open_modal = False  # Set open_modal to False when modal is closed
+        st.title("Employee Profile")
+        st.write(st.session_state.usr_prof)
+        if st.button("close"):
+            st.session_state.open_modal = False
+            st.rerun()
